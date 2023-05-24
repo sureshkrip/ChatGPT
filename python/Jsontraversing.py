95823c746212588ce7578d8b539c38e97dc6ed24
sureshkrip <sureshkrip@gmail.com>
2023-05-24 17:37:40 -0400

    test

--- python/Jsontraversing.py
+++ python/Jsontraversing.py
@@ -40,3 +40,32 @@ json_string = '{ "a": {"b": [{ "c":1, "b": [{"d": 2}], "e": [{"f": 3}] }]}}'
 data = json.loads(json_string)
 
 print(count_children(data))
+
+
+
+import json
+
+def count_keys(json_object):
+    if isinstance(json_object, dict):
+        return {k: count_keys(v) for k, v in json_object.items()}
+    elif isinstance(json_object, list):
+        return sum(count_keys(x) for x in json_object)
+    else:
+        return 0  # Base case: if not dict or list, it has no children
+
+def sum_counts(nested_dict):
+    if isinstance(nested_dict, dict):
+        return {key: sum_counts(value) for key, value in nested_dict.items()}
+    elif isinstance(nested_dict, int):
+        return nested_dict
+    else:  # it's a list
+        return sum(nested_dict)
+
+json_string = '{ "a": {"b": [{ "c":1, "b": [{"d": 2}], "e": [{"f": 3}] }]}}'
+data = json.loads(json_string)
+
+counts = count_keys(data)
+sums = sum_counts(counts)
+
+print(sums)
+
