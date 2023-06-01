1351bedaf6e457ac64626579651c5c5e5f039e31
sureshkrip <sureshkrip@gmail.com>
2023-06-01 14:42:51 -0400

    s3 file

---
+++ python/s3.py
@@ -0,0 +1,33 @@
+import boto3
+import zipfile
+import io
+
+def zip_s3_bucket_files(bucket_name, prefix, zip_file_name):
+    # Create a session using your AWS credentials
+    s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+
+    zip_buffer = io.BytesIO()
+
+    # Create a zip file in memory
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
+        for obj in s3.Bucket(bucket_name).objects.filter(Prefix=prefix):
+            # Download each file to local
+            file_buffer = io.BytesIO()
+            s3_client.download_fileobj(Bucket=bucket_name, Key=obj.key, Fileobj=file_buffer)
+            file_buffer.seek(0)
+
+            # Add file into zip file
+            zip_file.writestr(obj.key, file_buffer.read())
+
+    # Reset file pointer
+    zip_buffer.seek(0)
+    
+    # Create a new object and upload the zip file
+    s3.Object(bucket_name, f"{prefix}/{zip_file_name}").put(Body=zip_buffer)
+    
+bucket_name = "your_bucket_name" # replace with your bucket name
+prefix = "your_directory" # replace with your directory in bucket
+zip_file_name = "my_zip_file.zip" # replace with the name you want for your zip file
+
+zip_s3_bucket_files(bucket_name, prefix, zip_file_name)
